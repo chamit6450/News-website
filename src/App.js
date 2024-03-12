9767cd651beeda52260ce383360b17a226127641
chamit6450 <chamit6450@gmail.com>
2024-03-12 19:11:17 +0530

    completed

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import News from './news/News'
+export default function App() {
+  return (
+    <div>
+      <News/>
+    </div>
+  )
+}
---
+++ src/news/News.js
@@ -0,0 +1,97 @@
+import React, { useEffect, useState } from 'react'
+import Newsitem from './Newsitem'
+import "./News.css"
+export default function News() {
+    const [data,setData] = useState([])
+    const [search,setSearch] = useState("");
+    const [page,setPage] = useState(1)
+    const [totalPage,setTotalPage] = useState(1);
+    const [condition,setCondition] = useState(true)
+    const pageSize = 9;
+const fetchData =async ()=>{
+        const data =await fetch(`https://newsapi.org/v2/everything?q=${search}&page=${page}&pageSize=${pageSize}&from=2023-12-29&sortBy=publishedAt&apiKey=c97aae5e181d486e978484dec0c9ae20`)
+        const response = await data.json()
+        setData(response.articles)
+        setTotalPage(Math.ceil(response.totalResults/pageSize))
+     }
+     console.log(totalPage)
+useEffect(()=>{
+ if(page > 1){
+  setCondition(false)
+ }
+ if(page===1){
+  setCondition(true)
+ }
+ fetchData();
+ 
+},[page])
+const handleSubmit = (e)=>{
+    e.preventDefault();
+    fetchData()
+}
+const handlePrev=()=>{
+   if(page>1){
+    setPage(page-1)
+   }
+}
+const handleNext=()=>{
+  if(page < totalPage){
+    setPage(page+1)
+  }
+}
+const handleChange=(currentPage)=>{
+  setPage(currentPage)
+}
+const buttonRendering = () => {
+  const paginationButtons = [];
+  for (let i = 1; i <= totalPage; i++) {
+    paginationButtons.push(
+      <button key={i} onClick={() => handleChange(i)}>{i}</button>
+    );
+  }
+  return paginationButtons;
+};
+
+return (
+  <>
+    <form className='form' onSubmit={handleSubmit}> 
+      <input
+        placeholder='Enter search query'
+        className='input'
+        type="text"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <button className='btnn'>Search</button>
+    </form>
+
+    <div className='main'>
+      {data ? (
+        <>
+          <ul className='container'> 
+            {data.map((item, index) => (
+              <Newsitem
+                key={index}
+                title={item.title}
+                description={item.description}
+                img={item.urlToImage}
+                url={item.url}
+              />
+            ))}
+          </ul>
+
+          <div className='pagination'>
+            <button onClick={handlePrev} disabled={condition}>Prev</button>
+            {buttonRendering()}
+            <button onClick={handleNext}>Next</button>
+          </div>
+        </>
+      ) : (
+        <div>
+          <h1 className='query'>Please enter a query to make a search</h1>
+        </div>
+      )}
+    </div>
+  </>
+);
+}
